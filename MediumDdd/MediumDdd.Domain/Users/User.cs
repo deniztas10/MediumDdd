f9e04bbbbe9df10f8fa321d2b5036766f009a340
DenizTas <deniztasweb@gmail.com>
2020-06-25 18:30:06 +0300

    init2

---
+++ MediumDdd/MediumDdd.Domain/Users/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MediumDdd.Domain.Users
+{
+    public class User // TODO:: Aggregete Root
+    {
+        public virtual Guid? TenantId { get; private set; }
+
+        public virtual string UserName { get; private set; }
+
+        public virtual string Name { get; private set; }
+
+        public virtual string Surname { get; private set; }
+
+        public virtual string Email { get; private set; }
+
+        public virtual bool EmailConfirmed { get; private set; }
+
+        public virtual string PhoneNumber { get; private set; }
+
+        public virtual bool PhoneNumberConfirmed { get; private set; }
+
+    }
+}
